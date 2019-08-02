4e394395f33b4ba7949583e1d94777b00799662b
Cameron Dempsey <camerontdempsey@gmail.com>
2019-08-02 23:43:19 +1000

    Invoke Python implementation inside C implementation

--- c.c
+++ c.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     printf("Hello from C!\n");
+    int status = system("python3 python.py");
     printf("Goodbye from C!\n");
     return 0;
 }
