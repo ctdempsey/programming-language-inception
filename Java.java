ea6f975a2bed040e01986543b7fff8fa827a57b9
Cameron Dempsey <camerontdempsey@gmail.com>
2019-08-02 23:21:31 +1000

    Invoke C implementation inside Java implementation

--- Java.java
+++ Java.java
@@ -1,7 +1,16 @@
+import java.lang.ProcessBuilder;
+
 public class Java {
 
     public static void main(String[] args) {
         System.out.println("Hello from Java!");
+        try {
+            Process p = new ProcessBuilder().command("./c.exe").inheritIO().start();
+            int exitCode = p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
         System.out.println("Goodbye from Java!");
     }
 }
