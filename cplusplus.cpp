c96388812dd2fbf81db35493995d198a1b8a0ba2
Cameron Dempsey <camerontdempsey@gmail.com>
2019-08-03 00:42:09 +1000

    Invoke C# implementation inside C++ implementation

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
 int main() {
     cout << "Hello from C++!\n";
+    int status = system("./CSharp.exe");
     cout << "Goodbye from C++!\n";
     return 0;
 }
