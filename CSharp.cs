85ef46fc096fb3a75276c53cfb8fda366598ee95
Cameron Dempsey <camerontdempsey@gmail.com>
2019-08-03 00:36:49 +1000

    Create initial C# implementation

---
+++ CSharp.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CSharp
+{
+   class CSharp
+   {
+      public static void Main( string[] args )
+      {
+          Console.Write("Hello from C#!\n");
+          Console.Write("Goodbye from C#!\n");
+      }
+   }
+}
