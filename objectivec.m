5cf800bfb1e38c3510a32a6a99e79d3dc927aaf0
Cameron Dempsey <camerontdempsey@gmail.com>
2019-08-03 22:19:15 +1000

    Edit Objective-C implementation to output to STDOUT

--- objectivec.m
+++ objectivec.m
@@ -5,8 +5,10 @@ int main (int argc, const char * argv[])
     #define NSLog(FORMAT, ...) fprintf(stderr, "%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    NSLog (@"Hello from Objective-C!");
-    NSLog (@"Goodbye from Objective-C!");
+    // NSLog (@"Hello from Objective-C!");
+    // NSLog (@"Goodbye from Objective-C!");
+    printf("Hello from Objective-C!\n");
+    printf("Goodbye from Objective-C!\n");
     [pool drain];
 
     return 0;
