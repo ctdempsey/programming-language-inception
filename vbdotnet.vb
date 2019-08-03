909fe35146c29ec3e687d83d69da6f69d3ac7061
Cameron Dempsey <camerontdempsey@gmail.com>
2019-08-03 16:34:49 +1000

    Invoke JavaScript implementation inside VB.Net implementation

--- vbdotnet.vb
+++ vbdotnet.vb
@@ -1,8 +1,15 @@
 Imports System
+Imports System.Diagnostics
 
 Module Module1
     Sub Main()
         Console.WriteLine("Hello from VB.Net!")
+        Dim ProcessProperties As New ProcessStartInfo
+        ProcessProperties.FileName = "node"
+        ProcessProperties.Arguments = "javascript.js"
+        ProcessProperties.UseShellExecute = false
+        Dim process As Process = Process.Start(ProcessProperties)
+        process.WaitForExit()
         Console.WriteLine("Goodbye from VB.Net!")
     End Sub
 End Module
