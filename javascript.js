6c059c146fc414fd45b9bd600d92a808cbcb5280
Cameron Dempsey <camerontdempsey@gmail.com>
2019-08-03 20:27:25 +1000

    Invoke PHP implementation inside JavaScript implementation

--- javascript.js
+++ javascript.js
@@ -1,2 +1,9 @@
+const { spawn } = require('child_process');
 console.log("Hello from JavaScript!");
-console.log("Goodbye from JavaScript!");
+const child = spawn('php', ['php.php']);
+child.stdout.on('data', (data) => {
+    process.stdout.write(data.toString());
+});
+child.on('exit', (code) => {
+    console.log("Goodbye from JavaScript!");
+});
