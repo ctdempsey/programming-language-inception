5495e72662bd657047e867cce414ba7e80074246
Cameron Dempsey <camerontdempsey@gmail.com>
2020-02-07 00:43:36 +1100

    Invoke Go implementation inside Swift implementation

--- swift.swift
+++ swift.swift
@@ -1,2 +1,22 @@
-print("Hello from Swift!")
+import Foundation
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin.C
+#endif
+
+
+func main() {
+  defer {
+    let p = Process()
+    p.executableURL = URL(fileURLWithPath: "./go.exe")
+    p.standardError = Pipe()
+    do { try p.run() } catch {}
+    p.waitUntilExit()
+  }
+  print("Hello from Swift!")
+  fflush(stdout)
+}
+
+main()
 print("Goodbye from Swift!")
