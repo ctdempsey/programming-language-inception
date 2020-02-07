e2b90275fce03bbd138a2034e06d2c442fb85a68
Cameron Dempsey <camerontdempsey@gmail.com>
2020-02-07 23:49:27 +1100

    Invoke Pascal implementation inside D implementation

--- d.d
+++ d.d
@@ -1,7 +1,11 @@
-import std.stdio:writeln;
+import std.stdio;
+import std.process;
 
 void main()
 {
     writeln("Hello from D!");
+    stdout.flush();
+    auto pid = spawnProcess("./pascal.exe");
+    wait(pid);
     writeln("Goodbye from D!");
 }
