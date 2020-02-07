7bd31de2bcc4c6a64f9a58099f8b519014124125
Cameron Dempsey <camerontdempsey@gmail.com>
2020-02-07 20:59:05 +1100

    Invoke R implementation inside Go implementation

--- go.go
+++ go.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+    "os"
+    "log"
+    "fmt"
+    "os/exec"
+)
 
 func main() {
     fmt.Printf("Hello from Go!\n")
+    cmd := exec.Command("R", "--vanilla", "--slave", "-f", "r.r")
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        log.Fatal(err)
+    }
     fmt.Printf("Goodbye from Go!\n")
 }
