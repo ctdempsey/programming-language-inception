424c00c46b769fa10b676efd180a52ea1f6ccc67
Cameron Dempsey <camerontdempsey@gmail.com>
2020-02-08 20:21:21 +1100

    Invoke Scala implementation inside Dart implementation

--- dart.dart
+++ dart.dart
@@ -1,4 +1,14 @@
+import 'dart:io';
+import 'dart:convert';
+
 void main() {
   print('Hello from Dart!');
-  print('Goodbye from Dart!');
+  Process.start('scala', ['Scala']).then((Process process) {
+    process.stdout
+        .transform(utf8.decoder)
+        .listen((output) { print(output); });
+    process.exitCode.then((int code) {
+      print('Goodbye from Dart!');
+    });
+  });
 }
