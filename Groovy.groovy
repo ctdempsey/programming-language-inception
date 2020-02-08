922278802fa16f16cc29158308865dfb3a8cc0a1
Cameron Dempsey <camerontdempsey@gmail.com>
2020-02-08 22:30:25 +1100

    Invoke F# implementation inside Groovy implementation

--- Groovy.groovy
+++ Groovy.groovy
@@ -1,6 +1,8 @@
 class Groovy {
    static void main(String[] args) {
-      print('Hello from Groovy!\n');
+      print('Hello from Groovy!');
+      Process p = new ProcessBuilder().command("mono", "FSharp.exe").inheritIO().start();
+      int exitCode = p.waitFor();
       print('Goodbye from Groovy!');
    }
 }
