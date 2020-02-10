f690d521ddd48ce4fc1154493975c37304e3e048
Cameron Dempsey <camerontdempsey@gmail.com>
2020-02-10 22:18:18 +1100

    Invoke Lisp implementation inside Cobol implementation

--- cobol.cbl
+++ cobol.cbl
@@ -2,5 +2,6 @@
        PROGRAM-ID. HELLO.
        PROCEDURE DIVISION.
           DISPLAY 'Hello from Cobol!'.
+          CALL 'SYSTEM' USING 'sbcl --script lisp.lisp' END-CALL
           DISPLAY 'Goodbye from Cobol!'.
        STOP RUN.
