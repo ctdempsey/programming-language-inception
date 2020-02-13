7d557279029fc7d93783659ef1ab80d249bafbb6
Cameron Dempsey <camerontdempsey@gmail.com>
2020-02-14 00:56:45 +1100

    Invoke PowerShell implementation inside Fortran implementation

--- fortran.f90
+++ fortran.f90
@@ -2,6 +2,7 @@ program fortran
   implicit none
 
   print '(a)', 'Hello from Fortran!'
+  call execute_command_line ("pwsh powershell.ps1")
   print '(a)', 'Goodbye from Fortran!'
 
 end program fortran
