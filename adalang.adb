1e1200e93db5e41ed1d018e54f74311740c428f5
Cameron Dempsey <camerontdempsey@gmail.com>
2020-02-14 20:42:34 +1100

    Invoke Erlang implementation inside Ada implementation

--- adalang.adb
+++ adalang.adb
@@ -1,10 +1,26 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with GNAT.OS_Lib; use GNAT.OS_Lib;
 
 procedure AdaLang is
+  Result    : Boolean;
+  Arguments : Argument_List :=
+           (  1=> new String'("erl"),
+              2=> new String'("-noshell"),
+              3=> new String'("-run"),
+              4=> new String'("erlanglang"),
+              5=> new String'("-run"),
+              6=> new String'("init"),
+              7=> new String'("stop")
+           );
 
 begin
 
    Put_Line ("Hello from Ada!");
+   Spawn
+   (  Program_Name           => "/usr/bin/erl",
+      Args                   => Arguments,
+      Success                => Result
+   );
    Put_Line ("Goodbye from Ada!");
 
 end AdaLang;
